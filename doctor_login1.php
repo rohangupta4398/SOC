4b54d20ebd8c2142deb54d018068578af4a3f1a4
Paramraol <37235345+Paramraol@users.noreply.github.com>
2018-06-17 01:23:46 +0530

    Update doctor_login1.php

--- doctor_login1.php
+++ doctor_login1.php
@@ -42,7 +42,44 @@
          });
     </script>
     
-<style> 
+<style>
+    
+	table.w3-table-all {
+    margin: 20px 0;
+}
+    
+.w3-table-all {
+    border: 1px solid #ccc;
+}
+    
+.w3-table, .w3-table-all {
+    border-collapse: collapse;
+    border-spacing: 0;
+    width: 100%;
+    display: table;
+}
+    
+*, ::before, ::after {
+    box-sizing: inherit;
+}
+    
+.w3-white, .w3-hover-white:hover {
+    color: #000 !important;
+}
+    
+.w3-light-grey, .w3-hover-light-grey:hover, .w3-light-gray, .w3-hover-light-gray:hover {
+    color: #000 !important;
+}
+    
+html, body {
+    font-family: Verdana,sans-serif;
+    font-size: 15px;
+    line-height: 1.5;
+}
+    
+tr:nth-child(even) {background: #CCC}
+tr:nth-child(odd) {background: #FFF}
+    
 #panel,#flip,#panel1,#flip1,#panel2,#flip2,#flip3,#panel3 {
     padding: 5px;
     text-align: center;
@@ -89,15 +126,105 @@ div.intro{
 </div><br>
 <div id="flip1" style="background-color: white;text-align:center;font-size: 50px;width: 70%;align-self: center;color: #555555;font-family:Arial Unicode MS" class="boxes">CURRENT PATIENTS</div>
 <div id="panel1" style="width: 65%;" class="boxes">
-    <br><br><br><br>
+    <br><br>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "doc_current_cases";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $table_name=$_SESSION['username']."current";
+    $sql = "SELECT *FROM ".$table_name;
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+     // output data of each row
+     echo "<table class="w3-table-all notranslate" style="text-align: center"><tr><th>NAME</th><th>DISEASE DIAGNOSED</th><th>SEVERITY</th></tr>"
+        while($row = $result->fetch_assoc()) {
+         echo "<tr><td>" . $row["name"]. "</td><td>" . $row["disease"]. "</td><td>" . $row["severity"]. "</td></tr>";
+        }
+        echo "</table>"
+    } else {
+     echo "No Patient ";
+    }
+    $conn->close();
+    ?> 
+    <br><br>
 </div><br>
 <div id="flip2" style="text-align:center;font-size: 50px;width: 70%;align-self: center;color: #555555;font-family:Arial Unicode MS" class="boxes">SOLVED CASES</div>
 <div id="panel2" style="width: 65%;" class="boxes">
-    <br><br><br><br>
+    <br><br>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "doc_completed_cases";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $table_name=$_SESSION['username'];
+    $sql = "SELECT *FROM ".$table_name;
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      echo "<table class="w3-table-all notranslate" style="text-align: center"><tr><th>NAME</th><th>DISEASE DIAGNOSED</th><th>DISCHARGE DATE</th></tr>"
+      while($row = $result->fetch_assoc()) {
+         echo "<tr><td>" . $row["name"]. "</td><td>" . $row["disease"]. "</td><td>" . $row["discharge_date"]. "</td></tr>";
+     }
+     echo "</table>"
+    } else {
+        echo "No Patient ";
+    }
+    $conn->close();
+    ?> 
+    <br><br>
 </div><br>
 <div id="flip3" style="text-align:center;font-size: 50px;width: 70%;align-self: center;color: #555555;font-family:Arial Unicode MS" class="boxes">REFERED CASES</div>
 <div id="panel3" style="width: 65%;" class="boxes">
-    <br><br><br><br>
+    <br><br>
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "doc_refered_cases";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $table_name=$_SESSION['username']."refered";
+    $sql = "SELECT *FROM ".$table_name;
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+     echo "<table class="w3-table-all notranslate" style="text-align: center"><tr><th>NAME</th><th>DISEASE DIAGNOSED</th><th>DISCHARGE DATE</th></tr>"
+     while($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["name"]. "</td><td>"  . $row["discharge_date"]. "</td></tr>";
+        }
+        echo "</table>"
+    } else {
+     echo "No Patient ";
+    }
+    $conn->close();
+    ?> 
+    <br><br>
 </div>
 </center>
 
